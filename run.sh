f06853953399365b02793639b10b9dc3f9f14711
Daniel Hershcovich <danielh@cs.huji.ac.il>
2014-09-23 15:11:57 +0300

    Do not specify output dimension (determined automatically)

--- run.sh
+++ run.sh
@@ -13,5 +13,5 @@ outfile="models/rntn_wvecDim_${wvecDim}_step_${step}_2.bin"
 
 echo $outfile
 python3 runNNet.py --step $step --epochs $epochs --outFile $outfile \
-                --outputDim 5 --wvecDim $wvecDim 
+                --wvecDim $wvecDim 
 
